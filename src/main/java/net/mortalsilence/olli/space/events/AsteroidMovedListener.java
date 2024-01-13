40fe49d7a17d0210e522a4c7fe5963c2a44f9efe
BrabaxDD <155897950+BrabaxDD@users.noreply.github.com>
2024-01-13 16:26:11 +0100

    ...

---
+++ src/main/java/net/mortalsilence/olli/space/events/AsteroidMovedListener.java
@@ -0,0 +1,7 @@
+package net.mortalsilence.olli.space.events;
+
+import net.mortalsilence.olli.space.gameObjects.Asteroid;
+
+public interface AsteroidMovedListener {
+    public void astroidMoved(Asteroid asteroid);
+}
