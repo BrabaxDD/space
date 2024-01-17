f8b6d920640bb35f704d4fe7aa67f851aeea6a74
Klavierfloh <grund.georg@web.de>
2024-01-17 18:04:14 +0100

    simple AlienUFO Klasse hinzugefügt, fliegt in gerader Linie von einen zufälligen Punkt des linken Bildschirmrandes zu einem zufälligen Punkt auf der linken Seinte.

--- src/main/java/net/mortalsilence/olli/space/factorys/GameSceneFactory.java
+++ src/main/java/net/mortalsilence/olli/space/factorys/GameSceneFactory.java
@@ -1,11 +1,16 @@
 package net.mortalsilence.olli.space.factorys;
 
+import javafx.scene.control.Alert;
+import net.mortalsilence.olli.space.AsteroidsApplet;
+import net.mortalsilence.olli.space.gameObjects.AlienUFO;
 import net.mortalsilence.olli.space.gameObjects.Asteroid;
 import net.mortalsilence.olli.space.gameObjects.Spaceship;
 import net.mortalsilence.olli.space.scenes.Scene;
+import processing.core.PApplet;
 import processing.core.PVector;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class GameSceneFactory {
     public Scene buildGameScene(){
@@ -18,6 +23,8 @@ public class GameSceneFactory {
         a1.setPos(new PVector(200,200));
         a1.setSpeed(new PVector(0,0));
         s.addObject(a1);
+        AlienUFO Alu1 = new AlienUFO(new PVector(0,(int)AsteroidsApplet.asteroidsApplet.random(0, PApplet.DEFAULT_HEIGHT)) , new PVector(PApplet.DEFAULT_WIDTH,AsteroidsApplet.asteroidsApplet.random(PApplet.DEFAULT_WIDTH, PApplet.DEFAULT_HEIGHT)),s);
+        s.addObject(Alu1);
         return s;
     }
 }
---
+++ src/main/java/net/mortalsilence/olli/space/gameObjects/AlienUFO.java
@@ -0,0 +1,39 @@
+package net.mortalsilence.olli.space.gameObjects;
+
+import net.mortalsilence.olli.space.AsteroidsApplet;
+import net.mortalsilence.olli.space.scenes.Scene;
+import processing.core.PApplet;
+import processing.core.PVector;
+
+public class AlienUFO  extends GameObject{
+    //KOAM
+    //Attribute
+    private PVector pos;
+    private PVector speed;
+    private PVector endPos;
+    private Boolean direction = true;
+
+    //Objekt
+    public AlienUFO(PVector startPos, PVector enPos, Scene scene){
+        super(scene);
+        this.endPos = new PVector(-startPos.x+enPos.x,-startPos.y+enPos.y);
+        this.speed = endPos.normalize(speed);
+        this.pos = startPos;
+        this.speed.normalize();
+    }
+
+    @Override
+    public void render(){
+        AsteroidsApplet.asteroidsApplet.ellipse(pos.x,pos.y,20,20);
+        if(direction){
+            AsteroidsApplet.asteroidsApplet.line(this.pos.x, this.pos.y,this.pos.x+ this.speed.x*30,this.pos.y+ this.speed.y*30);
+        }
+    }
+
+    @Override
+    public void process(){
+        pos.add(speed);
+    }
+
+    public PVector getPos(){return pos;}
+}
