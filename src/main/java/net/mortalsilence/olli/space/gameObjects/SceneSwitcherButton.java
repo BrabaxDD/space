7bcafbd9634c00a311dccf2affe7e4de2ca67f89
BrabaxDD <155897950+BrabaxDD@users.noreply.github.com>
2024-01-17 18:54:40 +0100

    Main Menu

--- src/main/java/net/mortalsilence/olli/space/gameObjects/SceneSwitcherButton.java
+++ src/main/java/net/mortalsilence/olli/space/gameObjects/SceneSwitcherButton.java
@@ -4,6 +4,7 @@ import net.mortalsilence.olli.space.AsteroidsApplet;
 import net.mortalsilence.olli.space.events.MouseLeftClickListener;
 import net.mortalsilence.olli.space.factorys.GameSceneFactory;
 import net.mortalsilence.olli.space.scenes.Scene;
+import processing.core.PConstants;
 import processing.core.PVector;
 
 import java.util.ArrayList;
@@ -17,6 +18,10 @@ public class SceneSwitcherButton extends Button{
     @Override
     public void render(){
         super.render();
+        AsteroidsApplet.asteroidsApplet.fill(0);
+        AsteroidsApplet.asteroidsApplet.textSize(30);
+        AsteroidsApplet.asteroidsApplet.textAlign(PConstants.CENTER);
+        AsteroidsApplet.asteroidsApplet.text("Start Game", posLeftUp.x + this.diagonal.x/2 ,posLeftUp.y + this.diagonal.y/2 + 15);
     }
 
     @Override
